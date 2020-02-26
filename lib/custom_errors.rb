71e369ba789d031405ca96d436b6908b49d070d5
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-26 02:18:12 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -11,12 +11,10 @@ class Person
   end
   
   class PartnerError < StandardError
-end 
+  end 
 
 end
 
-class PartnerError < StandardError
-end 
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
